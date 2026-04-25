a2bc377eac125cfb8f6575819d25bacd31ea63e7
Andrew Kelley <andrew@ziglang.org>
2026-04-24 19:43:45 -0700

    declare linker test bankruptcy
    The active contributors and maintainers of Zig's linker code have
    generally found the current linker test harness to be cumbersome. The
    tests require a lot of maintenance, but do not provide a lot of
    coverage, and when they fail it is painful to troubleshoot.
    
    Furthermore, as part of working on #31691, I don't want to port over the
    CheckObject step, because I don't like the code anyway.
    
    The plan forward is to start enhancing `zig objdump` to assist in
    linker development, as well as using it as the basis for snapshot
    testing.
    
    We absolutely need linker test coverage, but we need to try to improve
    these things about the next attempt:
    * less effort to create and maintain tests
    * less CPU overhead - we should be able to add a lot of tests without
      adding a lot of CI time.
    * more helpful failures. A failed linker test should provide the next
      steps a developer can take to understand why the test failed.
    * a goal of porting over all of LLD's test suite, or at least the good
      ones.
    
    I'm not going to open an issue to track the lost linker test coverage,
    because there was already so much lack of coverage for linker stuff.
    
    However I will open issues to track this lost coverage:
    * the deleted checks from test/standalone/glibc_compat/build.zig
    * the deleted checks from test/standalone/compiler_rt_panic/build.zig
    * the deleted checks from test/standalone/ios/build.zig

--- build.zig
+++ build.zig
@@ -669,7 +669,6 @@ pub fn build(b: *std.Build) !void {
         .skip_llvm = skip_llvm,
         .max_rss = 3_300_000_000,
     }));
-    test_step.dependOn(tests.addLinkTests(b, enable_macos_sdk, enable_ios_sdk, enable_symlinks_windows));
     test_step.dependOn(tests.addStackTraceTests(b, test_filters, skip_non_native));
     test_step.dependOn(tests.addErrorTraceTests(b, test_filters, optimize_modes, skip_non_native));
     test_step.dependOn(tests.addCliTests(b));
