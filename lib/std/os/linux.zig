6413d10100512da921f3b8d589f0853b1de0a0cb
Alex Rønne Petersen <alex@alexrp.com>
2026-05-23 05:44:14 +0200

    Merge pull request 'std.zig.system.loongarch: take HWCAP into consideration' (#31614) from AstraFall/zig:loongarch/cpu-detect into master
    Reviewed-on: https://codeberg.org/ziglang/zig/pulls/31614
    Reviewed-by: Alex Rønne Petersen <alex@alexrp.com>

--- lib/std/os/linux.zig
+++ lib/std/os/linux.zig
@@ -96,7 +96,6 @@ pub fn clone(
 }
 
 pub const ARCH = arch_bits.ARCH;
-pub const HWCAP = arch_bits.HWCAP;
 pub const SC = arch_bits.SC;
 pub const VDSO = arch_bits.VDSO;
 pub const user_desc = arch_bits.user_desc;
@@ -522,6 +521,53 @@ pub const O = switch (native_arch) {
         _22: u10 = 0,
     },
     else => @compileError("missing std.os.linux.O constants for this architecture"),
+};
+
+pub const HWCAP = switch (native_arch) {
+    .arm, .armeb, .thumb, .thumbeb => struct {
+        pub const SWP = 1 << 0;
+        pub const HALF = 1 << 1;
+        pub const THUMB = 1 << 2;
+        pub const @"26BIT" = 1 << 3;
+        pub const FAST_MULT = 1 << 4;
+        pub const FPA = 1 << 5;
+        pub const VFP = 1 << 6;
+        pub const EDSP = 1 << 7;
+        pub const JAVA = 1 << 8;
+        pub const IWMMXT = 1 << 9;
+        pub const CRUNCH = 1 << 10;
+        pub const THUMBEE = 1 << 11;
+        pub const NEON = 1 << 12;
+        pub const VFPv3 = 1 << 13;
+        pub const VFPv3D16 = 1 << 14;
+        pub const TLS = 1 << 15;
+        pub const VFPv4 = 1 << 16;
+        pub const IDIVA = 1 << 17;
+        pub const IDIVT = 1 << 18;
+        pub const VFPD32 = 1 << 19;
+        pub const IDIV = IDIVA | IDIVT;
+        pub const LPAE = 1 << 20;
+        pub const EVTSTRM = 1 << 21;
+    },
+    .loongarch32, .loongarch64 => struct {
+        pub const CPUCFG = 1 << 0;
+        pub const LAM = 1 << 1;
+        pub const UAL = 1 << 2;
+        pub const FPU = 1 << 3;
+        pub const LSX = 1 << 4;
+        pub const LASX = 1 << 5;
+        pub const CRC32 = 1 << 6;
+        pub const COMPLEX = 1 << 7;
+        pub const CRYPTO = 1 << 8;
+        pub const LVZ = 1 << 9;
+        pub const LBT_X86 = 1 << 10;
+        pub const LBT_ARM = 1 << 11;
+        pub const LBT_MIPS = 1 << 12;
+        pub const PTW = 1 << 13;
+        pub const LSPW = 1 << 14;
+        pub const SCQ = 1 << 15;
+    },
+    else => struct {},
 };
 
 pub const RENAME = packed struct(u32) {
