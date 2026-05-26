b382630087cc15f0d0b6cece314e0ab44d6dc974
Alex Rønne Petersen <alex@alexrp.com>
2026-05-26 05:12:04 +0200

    std.os: no longer require libc for alpha-linux

--- lib/std/os.zig
+++ lib/std/os.zig
@@ -15,8 +15,6 @@ pub fn targetRequiresLibC(target: *const std.Target) bool {
     if (target.requiresLibC()) return true;
     return switch (target.os.tag) {
         .linux => switch (target.cpu.arch) {
-            // https://codeberg.org/ziglang/zig/issues/30940
-            .alpha,
             // https://codeberg.org/ziglang/zig/issues/30942
             .csky,
             // https://codeberg.org/ziglang/zig/issues/30943
