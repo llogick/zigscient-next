61e06d400c33eb269157ddf30074ca5a072c6f61
Zhalkhas <jalgas1998@gmail.com>
2026-05-28 04:32:23 +0200

    implicitly enable LLVM when TSan is enabled (#35311)
    FIxes #32127
    
    Reviewed-on: https://codeberg.org/ziglang/zig/pulls/35311

--- src/main.zig
+++ src/main.zig
@@ -4456,6 +4456,7 @@ fn createModule(
             error.DynamicLibraryPrecludesPie => fatal("dynamic libraries cannot be position independent executables", .{}),
             error.TargetRequiresPie => fatal("the specified target requires position independent executables", .{}),
             error.SanitizeThreadRequiresPie => fatal("thread sanitization requires position independent executables", .{}),
+            error.SanitizeThreadRequiresLlvmBackend => fatal("thread sanitization requires the LLVM backend", .{}),
             error.BackendLacksErrorTracing => fatal("the selected backend has not yet implemented error return tracing", .{}),
             error.LlvmLibraryUnavailable => fatal("zig was compiled without LLVM libraries", .{}),
             error.LldUnavailable => fatal("zig was compiled without LLD libraries", .{}),
