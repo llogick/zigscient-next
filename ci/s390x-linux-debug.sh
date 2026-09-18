6ccf7b1e1c1d9e6b458e6590eefaa6339d217342
Alex Rønne Petersen <alex@alexrp.com>
2026-09-18 11:12:40 +0200

    ci: switch s390x-linux mcpu from z15 to baseline
    This was the only CI platform that was using a non-baseline mcpu. I originally
    made this change thinking it made the CI runs faster, but I only later realized
    that the s390x VM hosts are badly oversubscribed, so the few runs I did aren't
    really useful data points. Also looking at the actual ISA features added between
    baseline (z13) and z15, they seem almost entirely irrelevant for a codebase like
    the Zig compiler.

--- ci/s390x-linux-debug.sh
+++ ci/s390x-linux-debug.sh
@@ -6,7 +6,7 @@ set -x
 set -e
 
 TARGET="s390x-linux-musl"
-MCPU="z15"
+MCPU="baseline"
 CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.17.0-dev.203+073889523"
 PREFIX="$HOME/deps/$CACHE_BASENAME"
 ZIG="$PREFIX/bin/zig"
