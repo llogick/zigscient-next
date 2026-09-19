f0eb123228bb1cde7f452612a7caf3de4c7a2c08
Alex Rønne Petersen <alex@alexrp.com>
2026-09-19 10:43:34 +0200

    std.os.linux: make errno() generic so that it can handle u32 and u64 correctly
    closes https://codeberg.org/ziglang/zig/issues/36883

--- lib/std/os/linux.zig
+++ lib/std/os/linux.zig
@@ -1039,10 +1039,9 @@ fn splitValue64(val: i64) [2]u32 {
 }
 
 /// Get the errno from a syscall return value. SUCCESS means no error.
-pub fn errno(r: u64) E {
-    const signed_r: i32 = @bitCast(@as(u32, @truncate(r)));
-    const int = if (signed_r > -4096 and signed_r < 0) -signed_r else 0;
-    return @fromBackingInt(@intCast(int));
+pub fn errno(r: anytype) E {
+    const signed_r: @Int(.signed, @typeInfo(@TypeOf(r)).int.bits) = @bitCast(r);
+    return @fromBackingInt(@intCast(if (signed_r > -4096 and signed_r < 0) -signed_r else 0));
 }
 
 pub fn brk(addr: usize) usize {
