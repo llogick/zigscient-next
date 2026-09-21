c383a20ea41df063c5d14019b5b77699824b9782
Andrew Kelley <andrew@ziglang.org>
2026-09-21 00:49:41 -0700

    std.Io.net.HostName: fix resolv.conf parsing overlong search lines
    This matches musl libc's behavior of skipping lines that contain
    too-long search values.
    
    Closes #35371

--- lib/std/Io/net/HostName.zig
+++ lib/std/Io/net/HostName.zig
@@ -484,6 +484,7 @@ pub const ResolvConf = struct {
                 },
                 .domain, .search => {
                     const rest = line_it.rest();
+                    if (rest.len > rc.search_buffer.len) continue;
                     @memcpy(rc.search_buffer[0..rest.len], rest);
                     rc.search_len = rest.len;
                 },
